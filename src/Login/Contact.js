ce9a66c2713912c7ea53fbadfe41f417c01270f8
Poornima Metri <poornima.m153@gmail.com>
2022-11-08 13:47:26 +0000

    Last FSD commit

---
+++ src/Login/Contact.js
@@ -0,0 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Contact = () => {
+
+    return (
+
+        <div class="container-xl" class="p-3 mb-2 bg-info text-white" >
+                <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                <h2>Contact Page</h2>
+                    <h1 class="display-4">Capstone Project - E-Healthcare</h1>
+                    <p class="lead">Project  : React / Asp.Net / SqlServer</p>
+                    <p class="lead">Submitted By : Poornima Metri</p>
+                    <p class="lead">Emaid_ID : poornima.metri@dell.com</p>
+                    <div id="map-container-google-3" class="z-depth-1-half map-container-3">
+                          <iframe src="https://maps.google.com/maps?q=warsaw&t=k&z=13&ie=UTF8&iwloc=&output=embed"  allowfullscreen ></iframe>
+                    </div>
+
+                </div>
+                </div>
+               
+                
+        </div> 
+    );
+}
+
+export default Contact;
